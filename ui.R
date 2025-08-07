f777645af54a1947d49d334d183f41c936dbc413
dtharvey <harvey@depauw.edu>
2025-08-07 11:26:07 -0400

    applying style guide
    applied project style guide to css file; adjusted text in well panels to avoid overflow

--- ui.R
+++ ui.R
@@ -1,4 +1,4 @@
-# ui for central limit theorm app
+# ui for central limit therom app
 
 ui = navbarPage("AC 3.0: Central Limit Theorem",
                 theme = shinytheme("journal"),
@@ -10,61 +10,68 @@ ui = navbarPage("AC 3.0: Central Limit Theorem",
                 
                 # open introduction tab
                 tabPanel("Introduction",
-                 fluidRow(
-                  withMathJax(),
-                  column(width = 6,
-                   wellPanel(
-                    includeHTML("text/introduction.html")
-                    )
-                    ),
-                   column(width = 6,
-                    align = "center",
-                     plotOutput("introplot", height = "700px")
-                     )
-                     )
+                         fluidRow(
+                           withMathJax(),
+                           column(width = 6,
+                                  wellPanel(
+                                    class = "scrollable-well",
+                                    div(
+                                      class = "html-fragment",
+                                      includeHTML("text/introduction.html")
+                                    )
+                                  )),
+                           column(width = 6,
+                                  align = "center",
+                                  plotOutput("introplot", height = "600px")
+                           )
+                         )
                 ), # close introduction tab
                 
                 # open first activity tab
                 tabPanel("The Central Limit Theorem",
-                 column(width = 6,
-                   wellPanel(
-                    includeHTML("text/activity1.html")
-                   )     
-                   ),
-                         
-                  column(width = 6,
-                   align = "center",
-                    splitLayout(
-                      selectInput(inputId = "dataset", 
-                                  label = "choose distribution",
-                                  choices = c("normal","binomial", "Poisson",
-                                              "uniform", "bimodal"),
-                                  selected = "normal", width = "200px",
-                                  selectize = FALSE),
-                      sliderInput(inputId = "size",
-                                  label = "samples to average (n)",
-                                  min = 2, max = 50, value = 2,
-                                  width = "400px"),
-                                ),
-                   plotOutput("activity1plot", height = "600px")
-                                ),
-                                
-                         
-  ), # close tabpanel for first activity
-  
-  # open tab for wrapping-up
-  tabPanel("Wrapping up",
-             column(width = 6,
-                    wellPanel(id = "wrapupPanel",
-                              style = "overflow-y:scroll; max-height: 700px",
-                              includeHTML("text/wrappingup.html")
-                    )
-             ),
-             column(width = 6,
-                    align = "center",
-                    plotOutput("wrapupplot", height = "700px")
-             )
-  ), # close tab for wrapping up
-  
-  ) # close navbar
+                         fluidRow(
+                           column(width = 6,
+                                  wellPanel(
+                                    class = "scrollable-well",
+                                    div(
+                                      class = "html-fragment",
+                                      includeHTML("text/activity1.html")
+                                    )
+                                  )),
+                           
+                           column(width = 6,
+                                  align = "center",
+                                  splitLayout(
+                                    selectInput(inputId = "dataset", 
+                                                label = "choose distribution",
+                                                choices = c("normal","binomial", "Poisson",
+                                                            "uniform", "bimodal"),
+                                                selected = "normal", width = "200px",
+                                                selectize = FALSE),
+                                    sliderInput(inputId = "size",
+                                                label = "samples to average (n)",
+                                                min = 2, max = 50, value = 2,
+                                                width = "400px"),
+                                  ),
+                                  plotOutput("activity1plot", height = "500px")
+                           ),
+                         )), # close tabpanel for first activity
+                
+                # open tab for wrapping-up
+                tabPanel("Wrapping up",
+                         fluidRow(
+                           column(width = 6,
+                                  wellPanel(
+                                    class = "scrollable-well",
+                                    div(
+                                      class = "html-fragment",
+                                      includeHTML("text/wrappingup.html")
+                                    )
+                                  )),
+                           column(width = 6,
+                                  align = "center",
+                                  plotOutput("wrapupplot", height = "600px")
+                           )
+                         )), # close tab for wrapping up
+) # close navbar
 
